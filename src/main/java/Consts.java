41ee51f6045ca75dc47e54205a1fa7d369276434
Ilia Deriglazov <m313am@gmail.com>
2022-09-14 14:30:55 -0400

    Added endpoints to consts file

---
+++ src/main/java/Consts.java
@@ -0,0 +1,10 @@
+public class Consts {
+    public static final String URL = "https://api.apilayer.com/currency_data/live?apikey=";
+    public static final String TOKEN = "OOnVHfUDeFEwCvcQbEQmUWIrRJtQ0rm1";
+    public static final String CHANGE_ENDPOINT = "/change";
+    public static final String CONVERT_ENDPOINT = "/convert";
+    public static final String HISTORICAL_ENDPOINT = "/historical";
+    public static final String LIST_ENDPOINT = "/list";
+    public static final String LIVE_ENDPOINT = "/live";
+    public static final String TIMEFRAME_ENDPOINT = "/timeframe";
+}
