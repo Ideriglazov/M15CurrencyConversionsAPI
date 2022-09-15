cc10bed8a587a25bd6fac3045768ce2a3850b443
Ilia Deriglazov <m313am@gmail.com>
2022-09-15 19:02:31 -0400

    Added IncorrectDateTest

---
+++ src/test/java/IncorrectDateTest.java
@@ -0,0 +1,18 @@
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+public class IncorrectDateTest {
+    private static Response response;
+    @Test
+    public void IncorrectDateTest() {
+        response = given().contentType("application/json").get(Consts.URL + Consts.HISTORICAL_ENDPOINT + "?apikey=" + Consts.TOKEN + "&source=USD&currencies=CAD,EUR,ILS,RUB&date=Wrong date");
+        System.out.println(response.asString());
+        //Expected to return the status code 302
+        //Line nelow does not work. Code 200 is returned
+        //response.then().statusCode(302);
+        response.then().body("error.code", equalTo(302));
+    }
+}
